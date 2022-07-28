2afdb34308b8c6b764273c07b174f00facdb469f
hoangdev01 <dbvhoang@gmail.com>
2022-07-28 20:45:35 +0700

    fix register

--- src/app/Http/Controllers/Auth/RegisterController.php
+++ src/app/Http/Controllers/Auth/RegisterController.php
@@ -77,22 +77,22 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function register(Request $request)
-    {
-        $this->validator($request->all())->validate();
+    // public function register(Request $request)
+    // {
+    //     $this->validator($request->all())->validate();
 
-        event(new Registered($user = $this->create($request->all())));
+    //     event(new Registered($user = $this->create($request->all())));
 
-        $this->guard()->login($user);
+    //     $this->guard()->login($user);
 
-        if ($response = $this->registered($request, $user)) {
-            return $response;
-        }
+    //     if ($response = $this->registered($request, $user)) {
+    //         return $response;
+    //     }
 
-        return $request->wantsJson()
-                    ? new JsonResponse([], 201)
-                    : redirect($this->redirectPath());
-    }
+    //     return $request->wantsJson()
+    //                 ? new JsonResponse([], 201)
+    //                 : redirect($this->redirectPath());
+    // }
 
     public function emailVerify(Request $request){
         
--- src/routes/web.php
+++ src/routes/web.php
@@ -27,16 +27,16 @@ Auth::routes(['verify' => true]);
 Route::get('/email/verify', function () {
     return view('auth.verify');
 })->middleware('auth')->name('verification.notice');
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+// Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+//     $request->fulfill();
  
-    return redirect('/home');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
+//     return redirect('/home');
+// })->middleware(['auth', 'signed'])->name('verification.verify');
+// Route::post('/email/verification-notification', function (Request $request) {
+//     $request->user()->sendEmailVerificationNotification();
  
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//     return back()->with('message', 'Verification link sent!');
+// })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
 Route::get('send-email', [SendEmailController::class, 'index']);
 
